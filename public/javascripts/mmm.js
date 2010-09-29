e6db85facc50021da347dc5b12072a1a5431f33e
Joren De Groof <info@jorendegroof.be>
2010-09-29 15:38:41 +0200

    Feed ticker

---
+++ public/javascripts/mmm.js
@@ -0,0 +1,20 @@
+$(function() {
+	$('.band_messages, .fan_messages').each(function() {
+		var $this = $(this)
+		var url = $this.attr('data-update-uri')
+		$.getJSON(url+'?callback=?', function(json) {
+			$.each(json.messages, function() {
+				$this.append('<li style="display: none" class="'+ this.network + '">' + this.message +' <a href="'+ this.link +'" target="_blank">view</a></li>');
+			});
+		});
+		
+		setInterval(function() {
+			var html = $this.find('li:first').html();
+			$this.find('li:first').fadeOut('slow', function() {
+				$(this).remove()
+				$this.find('li:nth(2)').fadeIn()
+			})
+			$this.append('<li style="display:none">' + html + '</li>');
+		}, 4000)
+	})	
+})
